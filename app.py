008d622c3c6ca8401c9b2108fcbbdb8d342375d2
Donal Carpenter <donal.carpenter@yahoo.com>
2015-03-31 15:43:10 +0100

    trying to connect to mongolab
    mongo issues
    
    mongo fails
    
    mongo
    
    mongo
    
    mongo...
    
    mongo...
    
    mongo
    
    remove hard coded connection settings

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from os import getenv
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {'DB': 'heroku_app35290664'}
+app.config['MONGODB_SETTINGS'] = {
+                                    'db': getenv('MONGOLAB_DB', 'fancied_lolcats'),
+                                    'username': getenv('MONGOLAB_USER'),
+                                    'password': getenv('MONGOLAB_PASSWORD'),
+                                    'host': getenv('MONGOLAB_HOST'),
+                                    'port': int(getenv('MONGOLAB_PORT', 0))
+                                }
 app.config["SECRET_KEY"] = "KeepThisS3cr3t123"
 app.debug = True
 
